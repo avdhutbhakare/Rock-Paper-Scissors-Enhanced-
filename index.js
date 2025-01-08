5ec1704b5ecf5276591e115ebfadf0cfe7815b25
avdhutbhakare <141303447+avdhutbhakare@users.noreply.github.com>
2025-01-08 19:04:44 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,26 +1,36 @@
-var randomNumber1= Math.floor(Math.random()*3)+1;
-var image1= "images/Image" + randomNumber1 + ".png";
-document.querySelectorAll("img")[0].setAttribute("src", image1);
+var imageSelection ={
+    "Stone":"images/Image3.png",
+    "Paper":"images/Image1.png",
+    "Scissors":"images/Image2.png"
+};
 
-var randomNumber2 = Math.floor(Math.random() * 3) + 1;
-var image2 = "images/Image" + randomNumber2 + ".png";
-document.querySelectorAll("img")[1].setAttribute("src", image2);
+function StartGame(){
+   
 
-var resultText = ""
+   var moves = ["Stone", "Paper", "Scissors"];
+   //Assigning random image for player 2
+   var randomNumber2 = Math.floor(Math.random() * 3);
+   var player2Choice = moves[randomNumber2];
+   document.querySelectorAll("img")[1].setAttribute("src", imageSelection[player2Choice]);
+   //Assigning image for player 1
+   var player1Choice = document.getElementById("playerSelection").value;
+   var imageForPlayer1 = imageSelection[player1Choice];
+   document.querySelectorAll("img")[0].setAttribute("src",imageForPlayer1);
 
-if (randomNumber1===1 && randomNumber2===2){ //paper and scissors
-    resultText="Player 2 Wins";
-}else if(randomNumber1===1 && randomNumber2===3){ //Paper ans scissors
-    resultText="Player 1 Wins";
-}else if (randomNumber1 === 2 && randomNumber2===1){ //Scissors and paper
-    resultText="Player 1 Wins";
-}else if (randomNumber1===2 && randomNumber2===3){ //Scissors and Stone
-    resultText="Player 2 Wins";
-}else if(randomNumber1===3 && randomNumber2===1){ //Stone and Paper
-    resultText="Player 2 Wins";
-}else if(randomNumber1===3 && randomNumber2===2){ //Stone and Scissors
-    resultText="player 1 Wins";
-}else{
-    resultText="Draw !!";
-}
-document.querySelector("h1").textContent=resultText;
+    if (player1Choice==="Paper" && player2Choice==="Scissors"){ //paper and scissors
+        document.querySelector("h1").innerHTML="Player 2 Wins";
+    }else if(player1Choice==="Paper" && player2Choice==="Stone"){ //paper and Stone
+        document.querySelector("h1").innerHTML = "Player 1 Wins";
+    }else if(player1Choice==="Scissors" && player2Choice==="Paper"){ //scissors and paper
+        document.querySelector("h1").innerHTML = "Player 1 Wins";
+    }else if(player1Choice==="Scissors" && player2Choice==="Stone"){ //scissors and stone
+        document.querySelector("h1").innerHTML = "Player 2 Wins";
+    }else if(player1Choice==="Stone" && player2Choice==="Paper"){ //stone and paper
+        document.querySelector("h1").innerHTML = "Player 2 Wins";
+    }else if(player1Choice==="Stone" && player2Choice==="Scissors"){ //stone and scissors
+        document.querySelector("h1").innerHTML = "Player 1 Wins";
+    }else{
+        document.querySelector("h1").innerHTML = "Draw";
+    }
+
+};
